76765be748b589828f23a86f68bd4eec60d136c4
Htet Aung Kyaw <htetaungkyaw372005@gmail.com>
2024-11-26 14:32:39 +0630

    코드 업데이트

--- src/main/java/org/mdt/busanproject/entity/Permission.java
+++ src/main/java/org/mdt/busanproject/entity/Permission.java
@@ -4,14 +4,21 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Set;
+
 @Entity
 @Getter
 @Setter
+@Table(name = "permissions")
 public class Permission {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true, nullable = false)
     private String name;
+
+    @ManyToMany(mappedBy = "permissions", fetch = FetchType.LAZY)
+    private Set<Role> roles;
 }
