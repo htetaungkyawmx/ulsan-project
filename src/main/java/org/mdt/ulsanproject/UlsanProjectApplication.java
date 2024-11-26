fc526c61e3a4d0eebe22e75999baabdf55263506
Htet Aung Kyaw <htetaungkyaw372005@gmail.com>
2024-11-26 16:11:44 +0630

    부산 MDT 프로젝트 부서

---
+++ src/main/java/org/mdt/ulsanproject/UlsanProjectApplication.java
@@ -0,0 +1,13 @@
+package org.mdt.ulsanproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlsanProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UlsanProjectApplication.class, args);
+    }
+
+}
