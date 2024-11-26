7d78e779774ff9faf1c8da6d120f4b10dc028270
Htet Aung Kyaw <htetaungkyaw372005@gmail.com>
2024-11-26 14:38:40 +0630

    코드 업데이트

--- src/main/java/org/mdt/busanproject/repository/PermissionRepository.java
+++ src/main/java/org/mdt/busanproject/repository/PermissionRepository.java
@@ -2,7 +2,11 @@ package org.mdt.busanproject.repository;
 
 import org.mdt.busanproject.entity.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
-    Permission findByName(String name);
+    Optional<Permission> findByName(String name);
 }
