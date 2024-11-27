718682fb912e4e7e70c0c2c6147d318fc7531b21
Htet Aung Kyaw <htetaungkyaw372005@gmail.com>
2024-11-27 14:39:07 +0630

    코드 업데이트

--- src/main/java/org/mdt/busanproject/entity/User.java
+++ src/main/java/org/mdt/busanproject/entity/User.java
@@ -9,7 +9,7 @@ import java.util.Set;
 @Entity
 @Getter
 @Setter
-@Table(name = "`user`") // Backticks prevent conflicts with SQL reserved keywords
+@Table(name = "`user`")
 public class User {
 
     @Id
