a05f4fb86faf0cdaa9cc5145f76994ea859f0989
Htet Aung Kyaw <htetaungkyaw372005@gmail.com>
2024-11-28 12:05:49 +0630

    변경하고 이전 코드를 교체하여 코드를 업데이트합니다

--- src/main/java/org/mdt/busanproject/repository/CompanyRepository.java
+++ src/main/java/org/mdt/busanproject/repository/CompanyRepository.java
@@ -1,6 +1,6 @@
 package org.mdt.busanproject.repository;
 
-import org.mdt.aioceaneye.model.Company;
+import org.mdt.busanproject.model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
