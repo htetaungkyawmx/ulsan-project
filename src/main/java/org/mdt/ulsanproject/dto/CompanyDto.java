3903c8137676bf61d82311d81c0c8a0ac7a41312
Htet Aung Kyaw <htetaungkyaw372005@gmail.com>
2024-12-02 15:04:40 +0630

    코드 업데이트

--- src/main/java/org/mdt/ulsanproject/dto/CompanyDto.java
+++ src/main/java/org/mdt/ulsanproject/dto/CompanyDto.java
@@ -1,4 +1,17 @@
 package org.mdt.ulsanproject.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CompanyDto {
+
+    private int id;
+    private String name;
+    private String address;
+    private String email;
+
 }
--- src/main/java/org/mdt/ulsanproject/dto/DroneDto.java
+++ src/main/java/org/mdt/ulsanproject/dto/DroneDto.java
@@ -1,4 +1,12 @@
 package org.mdt.ulsanproject.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DroneDto {
+
 }
