436fcce865708b9f0abc70aa24cc75501e3894e2
Htet Aung Kyaw <htetaungkyaw372005@gmail.com>
2024-12-02 16:57:43 +0630

    코드 업데이트

--- src/main/java/org/mdt/ulsanproject/controller/DocumentController.java
+++ src/main/java/org/mdt/ulsanproject/controller/DocumentController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/document")
 public class DocumentController {
     @Autowired
     private DocumentService documentService;
--- src/main/java/org/mdt/ulsanproject/controller/MaintenanceController.java
+++ src/main/java/org/mdt/ulsanproject/controller/MaintenanceController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/maintenance")
 public class MaintenanceController {
     @Autowired
     private MaintenanceService maintenanceService;
