ed7fb3bc249e0e019626415bf7479324e44fbbf0
Htet Aung Kyaw <htetaungkyaw372005@gmail.com>
2025-01-08 14:40:04 +0630

    Open Mavlink Client

--- src/main/java/org/mdt/ulsanproject/MavlinkClients.java
+++ src/main/java/org/mdt/ulsanproject/MavlinkClients.java
@@ -1,25 +1,25 @@
-//package org.mdt.ulsanproject;
-//
-//import org.mdt.ulsanproject.client.MavlinkClient;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class MavlinkClients implements CommandLineRunner {
-//	private final MavlinkClient mavlinkClient;
-//
-//	public MavlinkClients(MavlinkClient mavlinkClient) {
-//		this.mavlinkClient = mavlinkClient;
-//	}
-//
-//	public static void main(String[] args) {
-//		System.setProperty("server.port", "8001");  // Set port 8001
-//		SpringApplication.run(MavlinkClients.class, args);
-//	}
-//
-//	@Override
-//	public void run(String... args) {
-//		mavlinkClient.startListening();
-//	}
-//}
+package org.mdt.ulsanproject;
+
+import org.mdt.ulsanproject.client.MavlinkClient;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavlinkClients implements CommandLineRunner {
+	private final MavlinkClient mavlinkClient;
+
+	public MavlinkClients(MavlinkClient mavlinkClient) {
+		this.mavlinkClient = mavlinkClient;
+	}
+
+	public static void main(String[] args) {
+		System.setProperty("server.port", "8001");  // Set port 8001
+		SpringApplication.run(MavlinkClients.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		mavlinkClient.startListening();
+	}
+}
